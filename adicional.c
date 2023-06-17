b796f6bf8ea6cd5f714855cad1a2e7ea3d480826
Tamiris Coutinho <tami_coliveira@hotmail.com>
2023-06-17 04:24:06 -0300

    Create adicional.c

---
+++ adicional.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+
+    //pega as informações do usuário
+    //int x = get_int("x:");
+    //int y = get_int("y:");
+
+    long x = get_long("x:");
+    long y = get_long("y:");
+
+
+    //printf("%i\n", x + y);
+    printf("%li\n", x + y);
+}
